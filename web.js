46e882ee3db3a34016bec0074b8b018c9429592d
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2012-05-17 09:13:41 +0200

    error fix

--- web.js
+++ web.js
@@ -3,14 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function(){
-  //app.set('views', __dirname + '/views');
-  //app.set('view engine', 'ejs');
-  //app.use(express.favicon());
-  //app.use(express.logger('dev'));
   app.use(express.static(__dirname + '/public'));
-  //app.use(express.bodyParser());
-  //app.use(express.methodOverride());
-  //app.use(app.router);
 });
 
 app.get('/', function(request, response) {
