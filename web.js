57bc030f5c797e214a0397fd61c4dfcf06727396
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2012-05-17 09:23:35 +0200

    error fix

--- web.js
+++ web.js
@@ -1,14 +1,18 @@
-var express = require('express');
+var express = require('express'), http=require('http');
 
 var app = express.createServer(express.logger());
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
 app.post('/send_temp', function(request, response) {
-    response.send(request.body.json);
-};
+    response.send(request.body.json_data);
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
