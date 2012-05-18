64e53735bf647c5f71baaca3bf9b7d06762a415f
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2012-05-18 12:14:48 +0200

    removed /public/stt.html

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
 });
 
 app.get('/', function(request, response) {
@@ -11,7 +12,11 @@ app.get('/', function(request, response) {
 });
 
 app.post('/send_temp', function(request, response) {
-    response.send(request.body.json);
+    //response.writeHead(200, JSON.stringify({'Content-Type': 'application/json'}));
+    response.header("Content-Type", "application/json");
+    response.send(JSON.stringify({result: 'OK'}));
+    //console.log(JSON.stringify(request.headers));
+    console.log('Current Temperature: '+request.body.temp);
 });
 
 var port = process.env.PORT || 3000;
