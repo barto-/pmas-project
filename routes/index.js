c0bf6b7afc2314255d641ee9a8e13aec8a0cabd3
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2012-05-20 18:54:24 +0200

    fixed 'No database selected' error

--- routes/index.js
+++ routes/index.js
@@ -19,14 +19,9 @@ var mysql = require('mysql');
 var client = mysql.createClient({
   host: HOST,
   user: USERNAME,
-  password: PASSWORD
+  password: PASSWORD,
+  database: DATABASE
 });
-
-
-// If no callback is provided, any errors will be emitted as `'error'`
-// events by the client
-client.query('USE '+DATABASE);
-
 
 exports.send_temp = function(req, res){
 	res.header("Content-Type", "application/json");
