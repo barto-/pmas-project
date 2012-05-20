356e3bfb0e6de201843de5e18df94878656571da
Marco Bartolucci <marco.bartolucci2@studio.unibo.it>
2012-05-20 16:51:15 +0200

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@ function IsNumeric(data){
     return parseFloat(data)==data;
 }
 
-var HOST="us-cdbr-east.cleardb.com";
-var USERNAME="41e8ea6ce2694c";
-var PASSWORD="fbebad2f";
-var DATABASE="heroku_774d1336350dc77";
-/*
+var HOST;
+var USERNAME;
+var PASSWORD;
+var DATABASE;
+
 var connect_string_splitted = process.env.CLEARDB_DATABASE_URL.split(":");
 USERNAME = connect_string_splitted[1].split("//")[1];
 PASSWORD = connect_string_splitted[2].split("@")[0];
 HOST = connect_string_splitted[2].split("@")[1].split("/")[0];
 DATABASE = connect_string_splitted[2].split("@")[1].split("/")[1].split("?")[0];
-*/
+
+console.log("db_url: "+process.env.CLEARDB_DATABASE_URL);
 
 var mysql = require('mysql');
 
